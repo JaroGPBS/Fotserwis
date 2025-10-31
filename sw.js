863f33b6e0e6ac287af1672db43659b166fa5aef
Moorwen <moorwen1980@gmail.com>
2025-10-31 22:05:58 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('maszyny-v1').then((c)=>c.addAll([
+    './',
+    './index.html',
+    './manifest.webmanifest'
+  ])));
+});
+self.addEventListener('activate', (e)=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(
+    caches.match(e.request).then((r)=> r || fetch(e.request))
+  );
+});
