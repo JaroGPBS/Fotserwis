a17867017ee1bfaf477d0b1632d25d0abd3e9153
Moorwen <moorwen1980@gmail.com>
2025-11-07 18:01:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,11 @@
-// sw.js
-const CACHE_NAME = 'fotserwis-local-v7';
-const ASSETS = [
-  './',
-  './index.html?v=7',
-  './manifest.webmanifest?v=7',
-  './icon-192.png',
-  './icon-512.png'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open('fotserwis-local-v5').then((c)=>c.addAll([
+    './',
+    './index.html?v=7',
+    './manifest.webmanifest?v=5'
+  ])));
 });
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    (async () => {
-      // usuń stare cache
-      const keys = await caches.keys();
-      await Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve())));
-      await self.clients.claim();
-    })()
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
-  );
+self.addEventListener('activate', (e)=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', (e)=>{
+  e.respondWith(caches.match(e.request).then((r)=> r || fetch(e.request)));
 });
