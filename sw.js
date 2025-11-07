9c79d634d540e46d7783de1d7f9485107fd14f6a
Moorwen <moorwen1980@gmail.com>
2025-11-07 18:49:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,23 @@
+// sw.js v8
+const CACHE_NAME = 'fotserwis-local-v8';
+const ASSETS = [
+  './',
+  './index.html?v=8',
+  './manifest.webmanifest?v=8',
+  './icon-192.png',
+  './icon-512.png'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('fotserwis-local-v23').then((c)=>c.addAll([
-    './',
-    './index.html?v=23',
-    './manifest.webmanifest?v=23'
-  ])));
+  e.waitUntil(caches.open(CACHE_NAME).then((c)=>c.addAll(ASSETS)));
 });
-self.addEventListener('activate', (e)=>{ e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', (e)=>{
+self.addEventListener('activate', (e) => {
+  e.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve())));
+    await self.clients.claim();
+  })());
+});
+self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((r)=> r || fetch(e.request)));
 });
